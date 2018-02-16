cb9fe90b4a6f35a0147878f03eba6bfeea2cbfc3
gilalin <gil.alin@gmail.com>
2018-02-16 14:27:38 +0200

    Add files via upload

--- process.py
+++ process.py
@@ -1,9 +1,11 @@
 import os
-import psutils
+import psutil
+from multiprocessing import Process
 
 
 def send_cmd(cmd_name):
-	os.system(cmd_name)
+	return os.system(cmd_name)
+	 
 
 
 
@@ -18,5 +20,12 @@ def get_proc_pid(name):
 	
 if __name__ == '__main__':
 	cmd_list = ['copy c:\Users\Ulaf\Desktop\dir1\mov.MP4 C:\Users\Ulaf\Desktop\dir2','mkdir newdir']
-	send_cmd( [cmd_list[0] )
+	p = Process(target = send_cmd, args=(cmd_list[0],))
+	p.start()
+	
 	print get_proc_pid(cmd_list[0])
+	# '##############test################'
+	
+	p.join()
+	print 'done'
+	#print get_proc_pid(cmd_list[0])
